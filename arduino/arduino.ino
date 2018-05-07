4106eab8b7116b4142ee82f3bef757252cea078b
Jankele <31306582+Jankele@users.noreply.github.com>
2018-05-07 12:39:13 +0200

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -6,22 +6,36 @@ enum configurations
   pr = 1, pir, ble, pir_pr, pr_ble, pir_ble, pir_pr_ble
 };
 
+configurations configuration = (int)EEPROM.read(ADDRESS);
+
 void setup()
 {
-  configurations configuration = (int)(EEPROM.read(ADDRESS);
   if(configuration == pir || configuration == pir_pr || configuration == pir_ble || configuration == pir_pr_ble)
     pinMode(PIR, INPUT);
   if(configuration == pr || configuration == pir_pr || configuration == pr_ble || configuration == pir_pr_ble)
     pinMode(PR, INPUT);
   initiateLED();
   initiateConsole();
-  delay(2000);
 }
 
 void loop()
 {
-  loopPIR();
+  if(configuration == pir)
+    loopPIR();
+  if(configuration == pr)
   loopPR();
-  loopBLE();
+  if(configuration == ble)
+    loopBLE();
+  if(configuration == pir_pr)
+  {
+    loopPR();
+    loopPIR();
+  }
+  if(configuration == pr_ble)
+  {
+  {
+    loopPR();
+    loopBLE();
+  }
 }
 
