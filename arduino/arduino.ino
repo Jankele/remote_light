b3f2705e0ee9179ea1e2e338d675be0a3afb728a
Jankele <31306582+Jankele@users.noreply.github.com>
2018-05-11 13:43:09 +0200

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,50 +1,72 @@
 #include <EEPROM.h>
 #include "headers.h"
 
-typedef enum configurations
+unsigned long previousTime;
+unsigned long currentTime;
+
+enum configurations
 {
-  pr = 1, pir, ble, pir_pr, pr_ble, pir_ble, pir_pr_ble
+  EPR = 1, EPIR, EBLE, EPIR_PR, EPR_BLE, EPIR_BLE, EPIR_PR_BLE
 };
 
-configurations configuration = (int)EEPROM.read(ADDRESS);
+void initiateConsole();
 
 void setup()
 {
+  uint_8t configuration = (uint_8t)EEPROM.read(ADDRESS);
   if(configuration == pir || configuration == pir_pr || configuration == pir_ble || configuration == pir_pr_ble)
+  {
     pinMode(PIR, INPUT);
+    delay(1000); // kalibracja czujki pir
+  }
   if(configuration == pr || configuration == pir_pr || configuration == pr_ble || configuration == pir_pr_ble)
+  {
     pinMode(PR, INPUT);
+  }
   initiateLED();
   initiateConsole();
+  previousTime = millis();
 }
 
 void loop()
 {
-  if(configuration == pr)
+  currentTime = millis();
+  switch (configuration)
+  {
+    case EPR:
+      loopPR();
+    break;
+      
+    case EPIR: 
+      loopPIR();
+    break;
+      
+    case EBLE:
+      loopBLE();
+    break;
+      
+    case EPIR_PR
+    {
+      loopPR();
+      loopPIR();
+    }
+      
+    case EPR_BLE:
+      loopPR();
+      loopBLE();
+    break;
+    case EPIR_BLE:
     loopPR();
-  if(configuration == pir)
+    loopBLE();
+    loopPR();
     loopPIR();
-  if(configuration == ble)
-    loopBLE();
-  if(configuration == pir_pr)
-  {
-    loopPR();
-    loopPIR();
-  }
-  if(configuration == pr_ble)
-  {
-    loopPR();
     loopBLE();
   }
-  if(configuration == pir_ble)
-  {
-    loopPR();
-    loopBLE();
-  }
-  if(configuration == pir_pr_ble)
-  {
-    loopPR();
-    loopPIR();
-    loopBLE();
-  }
+  previousTime = millis();
+}
+
+void initiateConsole()
+{
+  Serial.begin(BAUD);
+  while(! Serial); //Czekanie na konsole
 }
