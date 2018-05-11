20f5b8650d3c2c387e7032784eb1266e3c681ed6
Jankele <31306582+Jankele@users.noreply.github.com>
2018-05-11 11:31:33 +0200

    Update pir.ino

--- arduino/pir.ino
+++ arduino/pir.ino
@@ -1,13 +1,9 @@
-loopPIR()
+void loopPIR()
 {
   static int licznik;
   if(licznik = 0)
   {
       if(digitalRead(PIR) == HIGH)
-    {
-      ledOn(LED1);
-      //x czasu
-      ledOff(LED1);
-    }
+        ledOn(LED1);
   }
 }
