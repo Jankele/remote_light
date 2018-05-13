e26938e1d1a886c2a0e18e5b99ea969b7db2d071
Jankele <jasio1991@gmail.com>
2018-05-13 13:50:38 +0200

    rearrange to work with millis

--- arduino/pir.ino
+++ arduino/pir.ino
@@ -1,7 +1,18 @@
-void handlePIR()
+void initiatePir()
+{
+  pinMode(PIR, INPUT);
+  delay(1000); // kalibracja czujki pir
+}
+
+bool handlePir()
 {
   if(digitalRead(PIR) == HIGH)
   {
     ledOn(LED1);
+    return true;
+  }
+  else
+  {
+    return false;
   }
 }
