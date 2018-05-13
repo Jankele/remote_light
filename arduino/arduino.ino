74b72bba2e7e5a50df9a562fb8ea8c3fe2008b65
Jankele <jasio1991@gmail.com>
2018-05-13 13:44:23 +0200

    add part of prototypes, group globals, clean up code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,84 +3,27 @@
 
 unsigned long previousTime;
 unsigned long currentTime;
-
-uint8_t configuration = (uint8_t)EEPROM.read(ADDRESS);
+uint8_t configuration;
+bool ledState;
+char serialHolder;
 
 void initiateConsole();
+void initiatePr();
+void initiatePir();
+void setTimer();
+void getTimer();
 
 void setup()
 {
-  pinMode(PIR, INPUT);
-  delay(1000); // kalibracja czujki pir
+  configuration = (uint8_t)EEPROM.read(ADDRESS);
 
-  pinMode(PR, INPUT);
-
-  initiateLED();
+  initiatePr();
+  initiatePir();
+  initiateLed();
   initiateConsole();
-
-  previousTime = millis();
 }
 
 void loop()
 {
-  currentTime = millis();
-  switch (configuration)
-  {
-    case EPR:
-      loopPR();
-    break;
-      
-    case EPIR: 
-      loopPIR();
-    break;
-      
-    case EBLE:
-      loopBLE();
-    break;
-      
-    case EPIR_PR
-    {
-      loopPR();
-      loopPIR();
-    }
-    break;
-
-    case EPR_BLE:
-    {
-      loopPR();
-      loopBLE();
-    }
-    break;
-
-    case EPIR_BLE:
-    {
-      loopPR();
-      loopBLE();
-    }
-    break;
-
-    case EPIR_PR_BLE:
-    {
-      loopPR();
-      loopPIR();
-      loopBLE();
-    }
-    break;
-
-    default:
-    {
-      loopPR();
-      loopPIR();
-      loopBLE();
-    }
-  }
-  previousTime = millis();
+  loopSwitch();
 }
-
-void initiateConsole()
-{
-  Serial.begin(BAUD);
-  while(! Serial); //Czekanie na konsole
-}
-
-
