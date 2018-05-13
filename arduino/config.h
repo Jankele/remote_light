2aee921cf4a2bfc6dd4fb1a624301ffce1c7220a
Jankele <jasio1991@gmail.com>
2018-05-13 13:49:34 +0200

    change name

---
+++ arduino/config.h
@@ -0,0 +1,23 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#define LED1 2
+#define LED2 3
+#define LED3 4
+#define BAUD 9600
+#define PR A0
+#define PIR 8
+#define POZIOM 300
+#define ADDRESS 0
+#define INTERVAL 1000
+
+//configurations
+#define EPR 1
+#define EPIR 2
+#define EBLE 3
+#define EPIR_PR 4
+#define EPR_BLE 5
+#define EPIR_BLE 6
+//----------
+
+#endif
