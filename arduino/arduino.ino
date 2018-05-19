4ea7c4793bc841db328f8cfe13896c8184dd0733
Jankele <jasio1991@gmail.com>
2018-05-19 17:16:04 +0200

    Deleted prototypes because version 1.8 of Arduino IDE handles prototypes automatically

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -14,29 +14,6 @@ bool isBleUsed;
 bool isLedTurnedOn;
 const unsigned long maxInterval = 1000;
 
-
-void ledOn(uint8_t Led);
-void initiateLed();
-void ledOff(uint8_t Led);
-void handlePir();
-bool isBright();
-void handlePr();
-void initiateConsole();
-void initiatePr();
-void initiatePir();
-void softReset();
-void handleBLE();
-void handleConfig();
-void loopEpir();
-void loopEble();
-void loopEpr_pir();
-void loopEpr_ble();
-void loopEpir_ble();
-void loopEpr_pir_ble();
-void loopSwitch();
-
-
-
 void setup()
 {
   configuration = (uint8_t)EEPROM.read(ADDRESS);
