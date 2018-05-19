a0565fdb6803c40ba0177c305d1f0e6a854f68ef
Jankele <jasio1991@gmail.com>
2018-05-19 17:10:32 +0200

    Declare all prototypes

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,20 +1,41 @@
 //TODO -review code
 #include <EEPROM.h>
-#include "headers.h"
+#include "config.h"
 
 unsigned long previousTime;
 unsigned long currentTime;
 unsigned long previousTimePir;
 unsigned long currentTimePir;
+unsigned long previousTimePr;
+unsigned long currentTimePr;
 uint8_t configuration;
 char serialHolder;
 bool isBleUsed;
-
+bool isLedTurnedOn;
 const unsigned long maxInterval = 1000;
 
+
+void ledOn(uint8_t Led);
+void initiateLed();
+void ledOff(uint8_t Led);
+void handlePir();
+bool isBright();
+void handlePr();
 void initiateConsole();
 void initiatePr();
 void initiatePir();
+void softReset();
+void handleBLE();
+void handleConfig();
+void loopEpir();
+void loopEble();
+void loopEpr_pir();
+void loopEpr_ble();
+void loopEpir_ble();
+void loopEpr_pir_ble();
+void loopSwitch();
+
+
 
 void setup()
 {
@@ -25,7 +46,7 @@ void setup()
   initiateLed();
   initiateConsole();
   previousTime = millis();
-  spreviousTimePir = millis();
+  previousTimePir = millis();
 }
 
 void loop()
